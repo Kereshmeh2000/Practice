f1cb2969a74965f21f72258e52056f886e80d2f3
kereshmeh <kereshmehsaadati@gmail.com>
2024-07-20 17:40:43 +0330

    Now I use the User model instead of fake data in the home side profile

--- resources/js/pages/home/Components/HomeSideProfile.tsx
+++ resources/js/pages/home/Components/HomeSideProfile.tsx
@@ -1,46 +1,63 @@
-import React from 'react';
+import React,{useState , useEffect} from 'react';
 import { MdOutlinePhotoLibrary } from "react-icons/md";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { TiEdit } from "react-icons/ti";
 import { FiArchive } from "react-icons/fi";
+import User from '../../../models/User';
 
 
 
 export default function HomeSideProfile () {
+    const [user, setUser] = useState<User[]>([]);
+
+    useEffect(() => {
+      const fetchUsers = async () => {
+        try {
+          const userList = await User.all();
+          setUser(userList);
+        } catch (error) {
+          console.error("Error fetching users:", error);
+        }
+      };
+  
+      fetchUsers();
+    }, []);
+
     return (
         <>
             <div className='hidden lg:block lg:w-1/4'>
-            <div className='border border-slate-100 py-8 px-12 text-center'>
-            <img src="https://picsum.photos/101/101" className='rounded-full mb-5 mx-auto' />
-            <p>User name</p>
-            <p>33, Divorced, İstanbul</p>
-        </div>
-        <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
-            <div className='mr-5 text-2xl'>
-                <MdOutlinePhotoLibrary />
+                <div className='border border-slate-100 py-8 px-12 text-center'>
+                        <div>
+                            <img src={user[0].profileImage} className='rounded-full mb-5 mx-auto' />
+                            <p>{user[0].name}</p>
+                            <p>33, Divorced, İstanbul</p>
+                        </div>
+                </div>
+                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
+                    <div className='mr-5 text-2xl'>
+                        <MdOutlinePhotoLibrary />
+                    </div>
+                    <p className='text-sm'>Photos</p>
+                </div>
+                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
+                    <div className='mr-5 text-2xl'>
+                        <FaRegCircleUser />
+                    </div>
+                    <p className='text-sm'>Main Info</p>
+                </div>
+                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
+                    <div className='mr-5 text-2xl'>
+                        <TiEdit />
+                    </div>
+                    <p className='text-sm'>Profile Text</p>
+                </div>
+                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
+                    <div className='mr-5 text-2xl'>
+                        <FiArchive />
+                    </div>
+                    <p className='text-sm'>My Archives</p>
+                </div>
             </div>
-            <p className='text-sm'>Photos</p>
-        </div>
-        <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
-            <div className='mr-5 text-2xl'>
-                <FaRegCircleUser />
-            </div>
-            <p className='text-sm'>Main Info</p>
-        </div>
-        <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
-            <div className='mr-5 text-2xl'>
-                <TiEdit />
-            </div>
-            <p className='text-sm'>Profile Text</p>
-        </div>
-        <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500'>
-            <div className='mr-5 text-2xl'>
-                <FiArchive />
-            </div>
-            <p className='text-sm'>My Archives</p>
-        </div>
-            </div>
-        
         </>
     )
 }
