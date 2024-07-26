41315db99130adb155df95af4928533f3a7b4727
kereshmeh <kereshmehsaadati@gmail.com>
2024-07-26 14:33:38 +0330

    photos modal

--- resources/js/components/ProfileSideBar.tsx
+++ resources/js/components/ProfileSideBar.tsx
@@ -1,24 +1,19 @@
-import React,{useState , useEffect} from 'react';
-import { MdOutlinePhotoLibrary } from "react-icons/md";
-import { FaRegCircleUser } from "react-icons/fa6";
-import { TiEdit } from "react-icons/ti";
-import { FiArchive } from "react-icons/fi";
+import React, { useState, useEffect } from 'react';
+import { MdOutlinePhotoLibrary } from 'react-icons/md';
+import { FaRegCircleUser } from 'react-icons/fa6';
+import { TiEdit } from 'react-icons/ti';
+import { FiArchive } from 'react-icons/fi';
 import User from '../models/User';
 import avatar from '../../../public/assets/images/avatar.png';
 import Modal from '../pages/profile/components/Modal';
+import { FaPlus } from 'react-icons/fa';
 
+export default function ProfileSideBar() {
+    const [isPhotoModalOpen, setIsPhotoModalOpen] = useState(false);
+    const [isInfoModalOpen, setIsInfoModalOpen] = useState(false);
+    const [isTextModalOpen, setIsTextModalOpen] = useState(false);
 
-
-export default function ProfileSideBar () {
-
-    //modal
-    const [showModal, setShowModal] = useState(false);
-    const handleModal = () => {
-        setShowModal(true);
-    }
-    const handleCloseModal = () => {
-        setShowModal(false);
-    }
+    //Main Info modal
 
     //getting the user data from the User model
     // const [user, setUser] = useState<User[]>([]);
@@ -32,51 +27,80 @@ export default function ProfileSideBar () {
     //       console.error("Error fetching users:", error);
     //     }
     //   };
-  
+
     //   fetchUsers();
     // }, []);
 
     return (
         <>
-            <div className='hidden lg:block lg:w-1/4 sticky top-0'>
-                <div className='border border-slate-100 py-8 px-12 text-center'>
-                        {/* <div key={user.id}>
-                            <img src={user.profileImage} className='rounded-full mb-5 mx-auto' />
-                            <p>{user.name}</p>
-                            <p>{user.location}</p>
-                        </div> */}
-                        <div>
-                            <img src={avatar}  />
-                            <p>User Name</p>
-                            <p>Isanbul</p>
-                        </div>
+            <div className="hidden lg:block lg:w-1/4 sticky top-0">
+                <div className="border border-slate-100 py-8 px-12 text-center">
+                    {/* <div key={user.id}>
+                        <img src={user.profileImage} className='rounded-full mb-5 mx-auto' />
+                        <p>{user.name}</p>
+                        <p>{user.location}</p>
+                    </div> */}
+                    <div>
+                        <img src={avatar} />
+                        <p>User Name</p>
+                        <p>Isanbul</p>
+                    </div>
                 </div>
-                <div onClick={handleModal} className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500 cursor-pointer'>
-                    <div className='mr-5 text-2xl'>
+
+                {/* Photos */}
+                <div
+                    onClick={() => setIsPhotoModalOpen(true)}
+                    className="border border-slate-100 p-5 flex items-center font-bold text-slate-500 cursor-pointer"
+                >
+                    <div className="mr-5 text-2xl">
                         <MdOutlinePhotoLibrary />
                     </div>
-                    <p className='text-sm'>Photos</p>
-                    {showModal && <Modal />}
+                    <p className="text-sm">Photos</p>
+                    {isPhotoModalOpen && (
+                        <Modal
+                            showModal={isPhotoModalOpen}
+                            closeModal={() => setIsPhotoModalOpen(false)}
+                            title="Photos"
+                            icon={<MdOutlinePhotoLibrary />}
+                        >
+                            <div>
+                                <div className="border border-gray-200 p-12 m-5 max-w-max">
+                                    <label htmlFor="file-upload" className="cursor-pointer">
+                                        <div className="text-3xl text-gray-200">
+                                            <FaPlus />
+                                        </div>
+                                        <input id="file-upload" type="file" className="hidden" />
+                                    </label>
+                                </div>
+                            </div>
+                            <p className="mt-10 text-xs">
+                                - You can upload up to 20 photos. When you want to upload a new photo, you need to delete one of the old photos.
+                            </p>
+                            <p className="text-xs">
+                                - The format of the photo you upload should be jpeg, jpg, gif, or png. The photo size cannot exceed 5 MB.
+                            </p>
+                        </Modal>
+                    )}
                 </div>
-                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500 cursor-pointer'>
-                    <div className='mr-5 text-2xl'>
+                <div className="border border-slate-100 p-5 flex items-center font-bold text-slate-500 cursor-pointer">
+                    <div className="mr-5 text-2xl">
                         <FaRegCircleUser />
                     </div>
-                    <p className='text-sm'>Main Info</p>
+                    <p className="text-sm">Main Info</p>
                 </div>
-                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500 cursor-pointer'>
-                    <div className='mr-5 text-2xl'>
+                <div className="border border-slate-100 p-5 flex items-center font-bold text-slate-500 cursor-pointer">
+                    <div className="mr-5 text-2xl">
                         <TiEdit />
                     </div>
-                    <p className='text-sm'>Profile Text</p>
+                    <p className="text-sm">Profile Text</p>
                 </div>
-                <div className='border border-slate-100 p-5 flex items-center font-bold	text-slate-500 cursor-pointer'>
-                    <div className='mr-5 text-2xl'>
+                <div className="border border-slate-100 p-5 flex items-center font-bold text-slate-500 cursor-pointer">
+                    <div className="mr-5 text-2xl">
                         <FiArchive />
                     </div>
-                    <p className='text-sm'>My Archives</p>
+                    <p className="text-sm">My Archives</p>
                 </div>
             </div>
         </>
-    )
+    );
 }
--- resources/js/pages/profile/components/Modal.tsx
+++ resources/js/pages/profile/components/Modal.tsx
@@ -1,46 +1,34 @@
 import React, { useState } from 'react';
 import { RxCross2 } from 'react-icons/rx';
-import { MdOutlinePhotoLibrary } from 'react-icons/md';
-import { FaPlus } from "react-icons/fa6";
 
+interface Modal{
+    showModal: boolean;
+    closeModal: () => void;
+    title: String;
+    children?: React.ReactNode;
+    icon: React.ReactNode;
+}
 
-export default function Modal() {
-    const [showModal, setShowModal] = useState(false);
-
-    const handleModal = () => {
-        setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
-
+export default function Modal({showModal, closeModal, title, children, icon}: Modal) {    
+    if (!showModal) {
+        return null;
+    }
     return (
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
-            <div className="bg-white rounded-lg w-1/2">
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-900 bg-opacity-50" onClick={closeModal}>
+            <div className="bg-white rounded-lg w-1/2" onClick={(e) => e.stopPropagation()}>
                 <div className="flex items-center justify-between p-4 border-b border-gray-200">
                     <div className="flex items-center justify-between">
                         <div className="mr-5 text-2xl">
-                            <MdOutlinePhotoLibrary />
+                            {icon}
                         </div>
-                        <p className="">Photos Edit</p>
+                        <p className="">{title}</p>
                     </div>
-                    <button className="text-2xl cursor-pointer" onClick={handleCloseModal}>
+                    <button className="text-2xl cursor-pointer" onClick={closeModal}>
                         <RxCross2 />
                     </button>
                 </div>
                 <div className='p-4'>
-                    <div>
-                        <div className='border border-gray-200 p-12 m-5 max-w-max'>
-                            <FaPlus />
-                        </div>
-                    </div>
-                    <p className="mt-10 text-xs">
-                        - You can upload up to 20 photos. When you want to upload a new photo, you need to delete one of the old photos.
-                    </p>
-                    <p className="text-xs">
-                        - The format of the photo you upload should be jpeg, jpg, gif, or png. The photo size cannot exceed 5 MB.
-                    </p>
+                    {children}
                 </div>
             </div>
         </div>
