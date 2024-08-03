d4cdcb3eb15f99bddea137dd5003cf208ede127a
kereshmeh <kereshmehsaadati@gmail.com>
2024-08-03 13:30:55 +0330

    added user data from model to search

--- resources/js/pages/search/search.tsx
+++ resources/js/pages/search/search.tsx
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
-import avatar from '../../../../public/assets/images/avatar.png';
+import React, {useState, useEffect} from 'react';
 import { FaStar, FaHeart } from "react-icons/fa";
 import { HiGift } from "react-icons/hi";
 import { HiEnvelope } from "react-icons/hi2";
 import { MdAddReaction, MdNotInterested } from "react-icons/md";
+import User from '../../models/User';
 
 export default function Search() {
+    // Tabs
     const [activeTab, setActiveTab] = useState('ONLINE MEMBERS');
     const handleTabChange = (tab) => {
         setActiveTab(tab);
     }
+
+    // Getting Users Data
+    const [users, setUsers] = useState<User[]>([]);
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const userList = await User.all();
+                setUsers(userList);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        }
+        fetchUsers();
+    }, []);
+
     return (
         <>
             <div className='px-3 md:px-20'>
@@ -24,24 +40,26 @@ export default function Search() {
                 </div>
                 {activeTab === 'ONLINE MEMBERS' && (
                     <div className='grid grid-cols-4 gap-4'>
-                        <div>
-                            <img src={avatar} alt="" />
-                            <div className='border border-slate-200'>
-                                <div className='text-center text-gray-600'>
-                                    <p className='font-bold pt-2'>User name</p>
-                                    <p>54, Divorced, İstanbul</p>
-                                    <p>online</p>
-                                </div>
-                                <div className='flex items-center justify-end [&>*]:p-2 text-gray-600'>
-                                    <button><FaStar /></button>
-                                    <button><FaHeart /></button>
-                                    <button><HiGift /></button>
-                                    <button><HiEnvelope /></button>
-                                    <button><MdAddReaction /></button>
-                                    <button><MdNotInterested /></button>
+                            {users.map (user => (
+                                <div key={user.id} className='border border-slate-200 mt-5'>
+                                <img src={user.profileImage} alt={user.name} className='w-full' />
+                                <div className='border-t border-slate-200'>
+                                    <div className='text-center text-gray-600'>
+                                        <p className='font-bold pt-2'>{user.name}</p>
+                                        <p>{user.location}</p>
+                                        <p>online</p>
+                                    </div>
+                                    <div className='flex items-center justify-end [&>*]:p-2 text-gray-600'>
+                                        <button><FaStar /></button>
+                                        <button><FaHeart /></button>
+                                        <button><HiGift /></button>
+                                        <button><HiEnvelope /></button>
+                                        <button><MdAddReaction /></button>
+                                        <button><MdNotInterested /></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                            ))}                            
                     </div>
                 )}
             </div>
